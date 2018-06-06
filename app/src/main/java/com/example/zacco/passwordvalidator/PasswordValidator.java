eb8add72df8174bc3bfbeecec8b5e91e68e351e6
zaco230 <zaccote@hotmail.com>
2018-06-06 20:35:18 -0300

    a2-stage2 UI

---
+++ app/src/main/java/com/example/zacco/passwordvalidator/PasswordValidator.java
@@ -0,0 +1,13 @@
+package com.example.zacco.passwordvalidator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PasswordValidator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_validator);
+    }
+}
